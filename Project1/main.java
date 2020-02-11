de3654585489336679b570e95267fac4b88b1ab6
Arthur Lee Jones <arthrjns@outlook.com>
2020-02-10 22:10:49 -0600

    Update main.java

--- Project1/main.java
+++ Project1/main.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ //System.out.println("Contents (Java 7)as : " + foundPOpeningTags + " " +  foundPClosingTags);
 package projectone;
 
 import java.io.File;
@@ -18,78 +19,89 @@ public class ProjectOneMain {
 
   public static void main(String[] args) throws Exception {
 
-    String htmlAsString = new String(Files.readAllBytes(Paths.get("C:\\Users\\Arthr\\Documents\\NetBeansProjects\\ProjectOne\\SecondExampleTable.html")));
-    String modifiedString = htmlAsString.toLowerCase();
-    ProjectOne objData;
-    objData = new ProjectOne();
+    String htmlAsString = new String(Files.readAllBytes(Paths.get("C:\\Users\\Arthr\\Documents\\NetBeansProjects\\ProjectOne\\Matador_Song_TTU.html"))); //File location
+    String modifiedString = htmlAsString.toLowerCase(); //Convert the string to lowercase.
+    ProjectOne objData = new ProjectOne(); //Start a new Class object
 
-    //Get the H1-6 tag positions (if any)
+
+    //Get title tag
+    int foundTitleOpeningTag = modifiedString.indexOf("<title>"); //Get the index of the opening title tag
+    int foundTitleClosingTag = modifiedString.indexOf("</title>"); //Get the index of the closing title tag
+    objData.setTitleTagPosition(0,foundTitleOpeningTag); //Store the starting title position inside our object
+    objData.setTitleTagPosition(1,foundTitleClosingTag); //Store the closing title position inside our object
+    objData.setTitleTagString(htmlAsString.substring(objData.getTitleTagPosition(0)+7,objData.getTitleTagPosition(1))); //Cut the string so it only contains the text inside the <title> tag and store it inside our object
+
+    //Loop through and get the H1-6 tag positions (if any)
     for(int i = 1; i <= 6; i++) {
       if(modifiedString.indexOf("<h"+i+">") > 0) {
-        objData.setHTagsPositions(i-1, 0, modifiedString.indexOf("<h"+i+">"));
-        objData.setHTagsPositions(i-1, 1, modifiedString.indexOf("</h"+i+">"));
-        objData.setHTagsStrings(htmlAsString.substring(objData.getHTagsPositions(i-1,0)+4,objData.getHTagsPositions(i-1,1)),i-1);
+        objData.setHTagsPositions(i-1, 0, modifiedString.indexOf("<h"+i+">")); //Store the <h1-6> opening position inside our object
+        objData.setHTagsPositions(i-1, 1, modifiedString.indexOf("</h"+i+">")); //Store the </h1-6> closing position inside our object
+        objData.setHTagsStrings(htmlAsString.substring(objData.getHTagsPositions(i-1,0)+4,objData.getHTagsPositions(i-1,1)),i-1); //Set the inner string values of each <h> tag.
       }
     }
 
     //Get the P tag postions (if there is multiple get those too)
-    int foundPOpeningTags = modifiedString.indexOf("<p>");
-    int foundPClosingTags = modifiedString.indexOf("</p>");
-    int pIndex = 0;
+    int foundPOpeningTags = modifiedString.indexOf("<p>"); //Get the index of the opening <p> tag
+    int foundPClosingTags = modifiedString.indexOf("</p>"); //Get the index of the closing <p> tag
+    int pIndex = 0; //Counter for how many p tags recived.
+    //Loop through the found opening tags
     while(foundPOpeningTags >= 0) {
-      objData.setPTagPositions(pIndex,0,foundPOpeningTags);
-      objData.setPTagPositions(pIndex,1,foundPClosingTags);
-      //System.out.println("Contents (Java 7)as : " + foundPOpeningTags + " " +  foundPClosingTags);
-      objData.setPTagsStrings(htmlAsString.substring(objData.getPTagsPositions(pIndex,0)+3,objData.getPTagsPositions(pIndex,1)),pIndex);
-      foundPOpeningTags = modifiedString.indexOf("<p>", foundPOpeningTags + 1);
-      foundPClosingTags = modifiedString.indexOf("</p>", foundPClosingTags + 1);
-      pIndex++;
+      objData.setPTagPositions(pIndex,0,foundPOpeningTags); //Store the opening tag position inside our object
+      objData.setPTagPositions(pIndex,1,foundPClosingTags); //Store the closing tag position insdie our object
+      objData.setPTagsStrings(htmlAsString.substring(objData.getPTagsPositions(pIndex,0)+3,objData.getPTagsPositions(pIndex,1)),pIndex); //Store the text inside the p tags inside our object
+      foundPOpeningTags = modifiedString.indexOf("<p>", foundPOpeningTags + 1); //Find the next opening tag
+      foundPClosingTags = modifiedString.indexOf("</p>", foundPClosingTags + 1); //Find the next closing tag
+      pIndex++; //++ to the counter
+    }
+    for(int i = 0; i < pIndex; i++) {
+      String formatedString = objData.getPTagsStrings(i);
+      formatedString = formatedString.replace("<br> ", "\n");
+      objData.setPTagsStrings(formatedString,i);
     }
 
-
     //Get the table data
-    int foundTDOpeningTags = modifiedString.indexOf("<td>");
-    int foundTDClosingTags = modifiedString.indexOf("</td>");
-    int tdIndex = 0;
+    int foundTDOpeningTags = modifiedString.indexOf("<td>"); //Get the index of the opening <td> tag
+    int foundTDClosingTags = modifiedString.indexOf("</td>"); //Get the index of the closing <td> tag
+    int tdIndex = 0; //Counter for how many td tags recived.
+    //Loop through the found opening tags
     while(foundTDOpeningTags >= 0) {
-      objData.setTDTagsPositions(tdIndex,0,foundTDOpeningTags);
-      objData.setTDTagsPositions(tdIndex,1,foundTDClosingTags);
-      objData.setTDTagsStrings(htmlAsString.substring(objData.getTDTagsPositions(tdIndex,0)+4,objData.getTDTagsPositions(tdIndex,1)),tdIndex);
-      foundTDOpeningTags = modifiedString.indexOf("<td>", foundTDOpeningTags + 1);
-      foundTDClosingTags = modifiedString.indexOf("</td>", foundTDClosingTags + 1);
-      tdIndex++;
+      objData.setTDTagsPositions(tdIndex,0,foundTDOpeningTags); //Store the opening tag position inside our object
+      objData.setTDTagsPositions(tdIndex,1,foundTDClosingTags); //Store the closing tag position insdie our object
+      objData.setTDTagsStrings(htmlAsString.substring(objData.getTDTagsPositions(tdIndex,0)+4,objData.getTDTagsPositions(tdIndex,1)),tdIndex); //Store the text inside the td tags inside our object
+      foundTDOpeningTags = modifiedString.indexOf("<td>", foundTDOpeningTags + 1); //Find the next opening tag
+      foundTDClosingTags = modifiedString.indexOf("</td>", foundTDClosingTags + 1); //Find the next closing tag
+      tdIndex++; //++ to the counter
     }
 
     //Remove any A tags from the TD tags.
     for(int i = 0; i < tdIndex; i++) {
-        String formatedString = objData.getTDTagsStrings(i);
-        int foundAOpeningTag = formatedString.indexOf(">");
-        int foundAClosingTag = formatedString.indexOf("</a>");
-        if(foundAOpeningTag > 0 || foundAClosingTag > 0) {
-          formatedString = formatedString.substring(0,foundAOpeningTag-1);
-          formatedString = formatedString.substring(foundAClosingTag,formatedString.length());
+        String formatedString = objData.getTDTagsStrings(i); //Get the text from the object
+        int foundAOpeningTag = formatedString.indexOf(">"); //Find the closing > for the <a element
+        int foundAClosingTag = formatedString.indexOf("</a>"); //Find the closing </a>
+        //If they both exist remove it and set the new string
+        if(foundAOpeningTag > 0 && foundAClosingTag > 0) {
+          formatedString = formatedString.substring(foundAOpeningTag+1,foundAClosingTag);
           objData.setTDTagsStrings(formatedString,i);
         }
-        System.out.println("Contents (Java 7)as : " + formatedString);
-
     }
 
 
     //Get the table header
-    int foundTableHeaderOpeningTags = modifiedString.indexOf("<th>");
-    int foundTableHeaderClosingTags = modifiedString.indexOf("</th>");
-    int tHeaderIndex = 0;
+    int foundTableHeaderOpeningTags = modifiedString.indexOf("<th>"); //Get the index of the opening <th> tag
+    int foundTableHeaderClosingTags = modifiedString.indexOf("</th>"); //Get the index of the closing <th> tag
+    int tHeaderIndex = 0; //Counter for how many th tags recived.
+    //Loop through the found opening tags
     while(foundTableHeaderOpeningTags >= 0) {
-      objData.setTableHeaderTagsPositions(tHeaderIndex,0,foundTableHeaderOpeningTags);
-      objData.setTableHeaderTagsPositions(tHeaderIndex,1,foundTableHeaderClosingTags);
-      objData.setTableHeaderTagsStrings(htmlAsString.substring(objData.getTableHeaderTagsPositions(tHeaderIndex,0)+4,objData.getTableHeaderTagsPositions(tHeaderIndex,1)),tHeaderIndex);
-      foundTableHeaderOpeningTags = modifiedString.indexOf("<th>", foundTableHeaderOpeningTags + 1);
-      foundTableHeaderClosingTags = modifiedString.indexOf("</th>", foundTableHeaderClosingTags + 1);
-      tHeaderIndex++;
+      objData.setTableHeaderTagsPositions(tHeaderIndex,0,foundTableHeaderOpeningTags); //Store the opening tag position inside our object
+      objData.setTableHeaderTagsPositions(tHeaderIndex,1,foundTableHeaderClosingTags); //Store the closing tag position insdie our object
+      objData.setTableHeaderTagsStrings(htmlAsString.substring(objData.getTableHeaderTagsPositions(tHeaderIndex,0)+4,objData.getTableHeaderTagsPositions(tHeaderIndex,1)),tHeaderIndex); //Store the text inside the th tags inside our object
+      foundTableHeaderOpeningTags = modifiedString.indexOf("<th>", foundTableHeaderOpeningTags + 1); //Find the next opening tag
+      foundTableHeaderClosingTags = modifiedString.indexOf("</th>", foundTableHeaderClosingTags + 1); //Find the next closing tag
+      tHeaderIndex++; //++ to the counter
     }
 
     //Print output to a File
-    File outFile = new File("output.txt");
+    File outFile = new File(objData.getTitleTagString() + ".txt");
     PrintStream output = new PrintStream(outFile);
 
     //Print H tags
